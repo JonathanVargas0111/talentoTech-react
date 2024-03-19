189e58c236dc3b16fb62437c8528aa27b62777de
felipe <jonathanfelipe0111@hotmail.com>
2024-03-19 18:56:32 -0500

    change footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,25 @@
+import { Link } from "react-router-dom";
 
-export default function Footer(){
+export default function Footer() {
 
     return (
-        <div className="bg-gray-800 text-white py-4 text-center  bottom-0 w-full">
-            <h1>Proyecto talento Tech</h1>
-            <h2 className="text-red-500">Desarrollador por Juanito</h2>
+        <div>
+            <footer class="bg-black pb-5">
+                <div class="max-w-screen-xl px-4 pt-8 mx-auto sm:px-6 lg:px-8">
+                    <div class="sm:flex sm:items-center sm:justify-between">
+                        <div class="flex justify-center text-gray-300 sm:justify-start">
+                            <ul className="flex px-8 space-x-5">
+                                <li><Link to='/' className="hover:text-blue-500">Inicio</Link></li>
+                                <li><Link to='/user' className="hover:text-blue-500">Usuarios</Link></li>
+                                <li><Link to='/create-user' className="hover:text-blue-500">Crear Usuario</Link></li>
+                            </ul>
+                        </div>
+                        <p class="mt-4 text-sm text-center text-gray-400 lg:text-right lg:mt-0">
+                            T&C &nbsp; Career &nbsp; Privacy & Policy &nbsp; Developer  for felipe vargas
+                        </p>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 }
